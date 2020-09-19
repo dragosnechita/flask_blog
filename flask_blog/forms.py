db7e6f17b70ea4fc8d01b8b76157d291cb1b6803
dragosnechita <nechitadragos@gmail.com>
2020-09-19 09:08:11 +0300

    Added the profile picture options and the forms to edit the account details

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
 	username = 	StringField('Username', 
@@ -24,7 +26,6 @@ class RegistrationForm(FlaskForm):
 		if user:
 			raise ValidationError('The email you chose is taken, please choose a different one')
 
-
 class LoginForm(FlaskForm):
 	email = StringField('Email', 
 		validators=[DataRequired(), Email()])
@@ -32,3 +33,23 @@ class LoginForm(FlaskForm):
 		validators=[DataRequired()])
 	remember = BooleanField('Remember me')
 	submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+	username = 	StringField('Username', 
+		validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', 
+		validators=[DataRequired(), Email()])
+	picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('The username you chose is taken, please choose a different one')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('The email you chose is taken, please choose a different one')
